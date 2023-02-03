56e3327cdc9adc1f8781a25cf77510924d59f7a3
MindW1n <ivan.prosworker@gmail.com>
2023-02-03 23:15:11 +0300

    0.05

--- src/kernel/main.asm
+++ src/kernel/main.asm
@@ -4,11 +4,11 @@ bits 64
 
 _startLM:
 	mov r9, done
-	call _print
+	call _print	
 	jmp _haltMachine
 
 %include "../../AsmFun/Headers64bit/Break/main.asm"
 %include "../../AsmFun/Headers64bit/HaltMachine/main.asm"
 %include "../../AsmFun/Headers64bit/Memcpy/main.asm"
 %include "src/drivers/screen/main.asm"
-done db "Done!", 10, 10, 10, "one", 10, 10, 10, "one", 10, 10, 10, "one", 10, "idkfjks", 10, "dkfs", 10, "dksfk", 10, "asjf;sajf;sdkf", 10, 10, 10, 10, 10, 10, "2432", 10, 10, "fsf", 10, "fskdf", 10, "fskddjfks", 0
+done db "Done!", 0
